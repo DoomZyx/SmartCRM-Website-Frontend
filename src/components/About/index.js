45e70902313ba98ef793f69b3d124618ced38aba
Axel Cella <160883113+DoomZyx@users.noreply.github.com>
2026-03-13 19:04:29 +0100

    chore: remove About section components and related styles from the project

--- src/components/About/index.js
+++
@@ -1,3 +0,0 @@
-export { default as StorySection } from "./StorySection/StorySection.jsx";
-export { default as TeamCard } from "./TeamCard/TeamCard.jsx";
-export { default as MissionSection } from "./MissionSection/MissionSection.jsx";
